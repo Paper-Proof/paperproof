987ec9c02cc9102a6a547a78605d617193426f5c
Anton Kovsharov <antonkovsharov@gmail.com>
2023-03-16 18:36:10 +0000

    Reproduce calling getInteractiveTermGoal in reaction to cursor update

--- widget/src/paperProof.tsx
+++ widget/src/paperProof.tsx
@@ -1,16 +1,42 @@
 import * as React from "react";
-import { EditorContext } from "@leanprover/infoview";
+import { useContext, useEffect, useState } from "react";
+import { EditorContext, RpcContext, useAsync } from "@leanprover/infoview";
+import {
+  TextDocumentPositionParams,
+  Location,
+} from "vscode-languageserver-protocol";
 
 export default function () {
-  const editorConnection = React.useContext(EditorContext);
+  const editorConnection = useContext(EditorContext);
+  const rs = useContext(RpcContext);
+  const [location, setLocation] = useState<Location | undefined>(undefined);
 
-  function onClick() {
-    editorConnection.api.insertText("-- Check widget wiring", "above");
-  }
+  useEffect(() => {
+    return editorConnection.events.changedCursorLocation.on((loc) => {
+      setLocation(loc);
+    }).dispose;
+  }, [rs]);
+
+  const response = useAsync(async () => {
+    if (!location) {
+      return Promise.reject();
+    }
+    const arg: TextDocumentPositionParams = {
+      textDocument: {
+        uri: location.uri,
+      },
+      position: location.range.start,
+    };
+    return rs.call("Lean.Widget.getInteractiveTermGoal", arg);
+  }, [location]);
 
   return (
     <div>
-      <button onClick={onClick}>insert</button>
+      {response.state === "loading"
+        ? "loading..."
+        : response.state === "rejected"
+        ? JSON.stringify(response.error)
+        : JSON.stringify(response.value)}
     </div>
   );
 }
