80745a63356a3c8db986330da06fe83679f8f9ad
Evgenia Karunus <lakesare@gmail.com>
2023-04-21 03:28:59 +0400

    Comment update

--- widget/rollup.config.js
+++ widget/rollup.config.js
@@ -40,9 +40,9 @@ const plugins = [
 ];
 
 export default (cliArgs) => {
-  // lakesare: We're compiling our tldraw code for ipad & vscode's extension separately, because:
+  // lakesare: We're compiling our tldraw code for browser & vscode's extension separately, because:
   // - vscode expects our code to be a module ({ format: "es" }, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules#javascript.statements.import)
-  // - we could add it to the ipad app via `<script type="module"/>`, but that would require externalizing ["react", "react-dom", "@leanprover/infoview"] somehow and I didn't want to google how to set this up. ALSO we would want 2 separate source files either way - one for ipad, `indexIpad.tsx` (that mounts the react component to out localhost:3000 html page); and one for the vscode extension, `indexVscode.tsx` (that just exports the react component).
+  // - we could add it to the browser app via `<script type="module"/>`, but that would require externalizing ["react", "react-dom", "@leanprover/infoview"] somehow and I didn't want to google how to set this up. ALSO we would want 2 separate source files either way - one for browser, `indexBrowser.tsx` (that mounts the react component to out localhost:3000 html page); and one for the vscode extension, `indexExtension.tsx` (that just exports the react component).
 
   if (cliArgs.configBrowser) {
     return {
