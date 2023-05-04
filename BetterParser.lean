b9111d3b5ad5e467151afa79dbc643f411b61b9f
Anton Kovsharov <antonkovsharov@gmail.com>
2023-05-04 18:07:15 +0100

    Add a check for non user tactics

--- BetterParser.lean
+++ BetterParser.lean
@@ -12,29 +12,49 @@ structure TacticApplication where
   tacticDependsOn : List String
   deriving Inhabited, ToJson
 
+def isUserTactic (t : Syntax) : Bool :=
+  if let .original .. := t.getHeadInfo then
+    true
+  else
+    false
+ 
 partial def parse : (infoTree : InfoTree) → IO (List TacticApplication) :=
   go none
 where go
   | some ctx, .node i cs => do
     if let .ofTacticInfo tInfo := i then
+      let getGoals := fun (goals : List MVarId) (mctx : MetavarContext) =>
+        goals.filterMap mctx.findDecl?
+            |>.mapM fun decl => do
+          let ppContext := ctx.toPPContext decl.lctx
+          return (← ppExprWithInfos ppContext decl.type).fmt.pretty
+
+      -- shortcut if it's not a tactic user wrote
+      if !isUserTactic tInfo.stx then
+        let as ← cs.toList.mapM (go <| i.updateContext? ctx)
+        return as.join
+ 
       match tInfo.stx with
       | `(tactic| apply $_)
       | `(tactic| exact $_)
       | `(tactic| refine $_)
       | `(tactic| sorry)
-      | `(tactic| linarith $_) =>
-        let getGoals := fun (goals : List MVarId) (mctx : MetavarContext) =>
-          goals.filterMap mctx.findDecl?
-             |>.mapM fun decl => do
-            let ppContext := ctx.toPPContext decl.lctx
-            return (← ppExprWithInfos ppContext decl.type).fmt.pretty
-
+      | `(tactic| linarith)
+      | `(tactic| intro $_:ident) =>
         return [{
           tacticName := s!"{tInfo.stx}",
           goalsBefore := ← getGoals tInfo.goalsBefore tInfo.mctxBefore,
           goalsAfter := ← getGoals tInfo.goalsAfter tInfo.mctxAfter,
           tacticDependsOn := []
           }]
+      | `(tactic| have $name : $_ := $_) =>
+        let as ← cs.toList.mapM (go <| i.updateContext? ctx)
+        return {
+          tacticName := s!"have {name}",
+          goalsBefore := ← getGoals tInfo.goalsBefore tInfo.mctxBefore,
+          goalsAfter := ← getGoals tInfo.goalsAfter tInfo.mctxAfter,
+          tacticDependsOn := []
+          } :: as.join
       | _ =>
         let as ← cs.toList.mapM (go <| i.updateContext? ctx)
         return as.join
