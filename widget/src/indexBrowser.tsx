c062518bf38cae24fd3d45deb5b19d06b5b05da2
Anton Kovsharov <antonkovsharov@gmail.com>
2023-05-18 16:12:31 +0100

    Wire indexBrowser to query server periodically

--- widget/src/indexBrowser.tsx
+++ widget/src/indexBrowser.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
+import { useEffect } from "react";
 import * as ReactDOM from "react-dom";
 import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/editor.css";
 import "@tldraw/tldraw/ui.css";
 
-ReactDOM.render(
-  <React.StrictMode>
+let lastId = 0;
+
+function App() {
+  useEffect(() => {
+    function getTypes() {
+      fetch("getTypes")
+        .then((response) => response.json())
+        .then((res) => {
+          const proofTree = res.data;
+          const id = Number(res.id);
+          if (id > lastId) {
+            console.log(id, proofTree);
+            lastId = id;
+          }
+        })
+        .catch((e) => {
+          console.log("error", e);
+        });
+    }
+    const interval = setInterval(getTypes, 200);
+    return () => clearInterval(interval);
+  }, []);
+  return (
     <div
       style={{
         position: "fixed",
@@ -14,6 +36,12 @@ ReactDOM.render(
     >
       <Tldraw />
     </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
