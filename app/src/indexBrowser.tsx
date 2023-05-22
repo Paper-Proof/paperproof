306cbdcdbdc24b45fbbaaf8b592e889f4750e976
Anton Kovsharov <antonkovsharov@gmail.com>
2023-05-22 18:11:20 +0100

    Display the goal at the cursor as the curos moved

--- app/src/indexBrowser.tsx
+++ app/src/indexBrowser.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import * as ReactDOM from "react-dom";
 import {
@@ -115,7 +116,6 @@ const exampleHaveWithPattern: Tree = {
   ],
 };
 
-
 const example: Tree = {
   nodes: [
     [
@@ -174,165 +174,176 @@ const example: Tree = {
   ],
 };
 
-export default function Example() {
-  const handleMount = (app: App) => {
-    app.updateInstanceState({ isFocusMode: true });
+function render(app: App, proofTree: string) {
+  console.log("Handle mount called");
+  app.updateInstanceState({ isFocusMode: true });
 
-    const inBetweenMargin = 20;
-    const framePadding = 20;
+  const inBetweenMargin = 20;
+  const framePadding = 20;
 
-    type Size = [number, number];
-    const { nodes, tactics } = example;
+  type Size = [number, number];
+  const { nodes, tactics } = example;
 
-    function vStack(margin: number, ...boxes: Size[]): Size {
-      const w = Math.max(...boxes.map((b) => b[0]));
-      const h = boxes.map((b) => b[1]).reduce((x, y) => x + y);
-      return [w, h + (boxes.length - 1) * margin];
-    }
+  function vStack(margin: number, ...boxes: Size[]): Size {
+    const w = Math.max(...boxes.map((b) => b[0]));
+    const h = boxes.map((b) => b[1]).reduce((x, y) => x + y);
+    return [w, h + (boxes.length - 1) * margin];
+  }
 
-    function hStack(margin: number, ...boxes: Size[]): Size {
-      const w = boxes.map((b) => b[0]).reduce((x, y) => x + y);
-      const h = Math.max(...boxes.map((b) => b[1]));
-      return [w + (boxes.length - 1) * margin, h];
-    }
+  function hStack(margin: number, ...boxes: Size[]): Size {
+    const w = boxes.map((b) => b[0]).reduce((x, y) => x + y);
+    const h = Math.max(...boxes.map((b) => b[1]));
+    return [w + (boxes.length - 1) * margin, h];
+  }
 
-    const drawNode = (
-      parentId: TLParentId | undefined,
-      text: string,
-      [x, y]: [number, number],
-      type: "value" | "tactic" = "value"
-    ): Size => {
-      const [w, h] = getSize({ text, id: "aa" });
-      app.createShapes([
-        {
-          id: app.createShapeId(),
-          type: "geo",
-          x,
-          y,
-          parentId,
-          props: {
-            geo: "rectangle",
-            w,
-            h,
-            ...(type == "value"
-              ? { dash: "draw", fill: "solid", color: "light-green" }
-              : { dash: "dotted", fill: "none", color: "grey" }),
-            size: "m",
-            text,
-          },
+  const drawNode = (
+    parentId: TLParentId | undefined,
+    text: string,
+    [x, y]: [number, number],
+    type: "value" | "tactic" = "value"
+  ): Size => {
+    const [w, h] = getSize({ text, id: "aa" });
+    app.createShapes([
+      {
+        id: app.createShapeId(),
+        type: "geo",
+        x,
+        y,
+        parentId,
+        props: {
+          geo: "rectangle",
+          w,
+          h,
+          ...(type == "value"
+            ? { dash: "draw", fill: "solid", color: "light-green" }
+            : { dash: "dotted", fill: "none", color: "grey" }),
+          size: "m",
+          text,
         },
-      ]);
-      return [w, h];
-    };
+      },
+    ]);
+    return [w, h];
+  };
 
-    function getTreeSize(layers: NodeLayer[]): [number, number] {
-      return vStack(
-        inBetweenMargin,
-        ...layers.map((l) => hStack(inBetweenMargin, ...l.map(getSize)))
-      );
+  function getTreeSize(layers: NodeLayer[]): [number, number] {
+    return vStack(
+      inBetweenMargin,
+      ...layers.map((l) => hStack(inBetweenMargin, ...l.map(getSize)))
+    );
+  }
+
+  function getFrameSize(layers: NodeLayer[]): [number, number] {
+    const size = getTreeSize(layers);
+    return [size[0] + framePadding * 2, size[1] + framePadding * 2];
+  }
+
+  function getTextSize(text: string): [number, number] {
+    const size = app.textMeasure.measureText({
+      ...TEXT_PROPS,
+      text,
+      fontFamily: "draw",
+      fontSize: LABEL_FONT_SIZES["m"],
+      width: "fit-content",
+      padding: "16px",
+    });
+    return [
+      // Don't know how to calculate size correctly yet
+      size.w * 1.3,
+      size.h,
+    ];
+  }
+
+  function getSize(node: Node): [number, number] {
+    const sizes: Size[] = [];
+    const [toValueTactic, fromValueTactic] = getTactics(node, tactics);
+    if (toValueTactic) {
+      sizes.push(getTextSize(toValueTactic.text));
     }
-
-    function getFrameSize(layers: NodeLayer[]): [number, number] {
-      const size = getTreeSize(layers);
-      return [size[0] + framePadding * 2, size[1] + framePadding * 2];
+    sizes.push(getTextSize(node.text));
+    if (fromValueTactic) {
+      sizes.push(getTextSize(fromValueTactic.text));
     }
+    const frameSize: Size = node.subNodes
+      ? getFrameSize(node.subNodes)
+      : [0, 0];
+    return vStack(0, frameSize, ...sizes);
+  }
+  app.selectAll().deleteShapes();
 
-    function getTextSize(text: string): [number, number] {
-      const size = app.textMeasure.measureText({
-        ...TEXT_PROPS,
-        text,
-        fontFamily: "draw",
-        fontSize: LABEL_FONT_SIZES["m"],
-        width: "fit-content",
-        padding: "16px",
-      });
-      return [
-        // Don't know how to calculate size correctly yet
-        size.w * 1.3,
-        size.h,
-      ];
-    }
+  function getTactics(
+    node: Node,
+    tactics: Tactic[]
+  ): [Tactic | undefined, Tactic | undefined] {
+    const toValueTactic = tactics.find(
+      (t) => t.toNodeIds.length == 1 && t.toNodeIds.includes(node.id)
+    );
+    const fromValueTactic = tactics.find(
+      (t) => t.fromNodeIds.length == 1 && t.fromNodeIds.includes(node.id)
+    );
+    return [toValueTactic, fromValueTactic];
+  }
 
-    function getSize(node: Node): [number, number] {
-      const sizes: Size[] = [];
-      const [toValueTactic, fromValueTactic] = getTactics(node, tactics);
-      if (toValueTactic) {
-        sizes.push(getTextSize(toValueTactic.text));
-      }
-      sizes.push(getTextSize(node.text));
-      if (fromValueTactic) {
-        sizes.push(getTextSize(fromValueTactic.text));
-      }
-      const frameSize: Size = node.subNodes
-        ? getFrameSize(node.subNodes)
-        : [0, 0];
-      return vStack(0, frameSize, ...sizes);
-    }
-    app.selectAll().deleteShapes();
-
-    function getTactics(
-      node: Node,
-      tactics: Tactic[]
-    ): [Tactic | undefined, Tactic | undefined] {
-      const toValueTactic = tactics.find(
-        (t) => t.toNodeIds.length == 1 && t.toNodeIds.includes(node.id)
-      );
-      const fromValueTactic = tactics.find(
-        (t) => t.fromNodeIds.length == 1 && t.fromNodeIds.includes(node.id)
-      );
-      return [toValueTactic, fromValueTactic];
-    }
-
-    function drawNodes(
-      parentId: TLParentId | undefined,
-      [x0, y0]: [number, number],
-      layers: NodeLayer[]
-    ) {
-      let y = y0;
-      for (const layer of layers) {
-        let x = x0;
-        for (const node of layer) {
-          const sizes: Size[] = [];
-          let dy = 0;
-          const [toValueTactic, fromValueTactic] = getTactics(node, tactics);
-          if (toValueTactic) {
-            const size: Size = drawNode(
-              parentId,
-              toValueTactic.text,
-              [x, y],
-              "tactic"
-            );
-            sizes.push(size);
-            dy += size[1];
-          }
-          const valueSize = drawNode(parentId, node.text, [x, y + dy]);
-          sizes.push(valueSize);
-          dy += valueSize[1];
-          if (fromValueTactic) {
-            const size: Size = drawNode(
-              parentId,
-              fromValueTactic.text,
-              [x, y + dy],
-              "tactic"
-            );
-            sizes.push(size);
-            dy += size[1];
-          }
-
-          x += vStack(0, ...sizes)[0] + inBetweenMargin;
+  function drawNodes(
+    parentId: TLParentId | undefined,
+    [x0, y0]: [number, number],
+    layers: NodeLayer[]
+  ) {
+    let y = y0;
+    for (const layer of layers) {
+      let x = x0;
+      for (const node of layer) {
+        const sizes: Size[] = [];
+        let dy = 0;
+        const [toValueTactic, fromValueTactic] = getTactics(node, tactics);
+        if (toValueTactic) {
+          const size: Size = drawNode(
+            parentId,
+            toValueTactic.text,
+            [x, y],
+            "tactic"
+          );
+          sizes.push(size);
+          dy += size[1];
         }
-        y += getTreeSize([layer])[1] + inBetweenMargin;
+        const valueSize = drawNode(parentId, node.text, [x, y + dy]);
+        sizes.push(valueSize);
+        dy += valueSize[1];
+        if (fromValueTactic) {
+          const size: Size = drawNode(
+            parentId,
+            fromValueTactic.text,
+            [x, y + dy],
+            "tactic"
+          );
+          sizes.push(size);
+          dy += size[1];
+        }
+
+        x += vStack(0, ...sizes)[0] + inBetweenMargin;
       }
+      y += getTreeSize([layer])[1] + inBetweenMargin;
     }
+  }
 
-    drawNodes(undefined, [0, 0], nodes);
+  console.log("draw node", proofTree);
+  drawNode(undefined, proofTree, [0, -100]);
+  drawNodes(undefined, [0, 0], nodes);
+}
 
+export default function Example({ proofTree }: { proofTree: string | null }) {
+  console.log("Example called");
+  const [app, setApp] = useState<App | null>(null);
+  if (app) {
+    render(app, proofTree ?? "No value");
+  }
+  const handleMount = (app: App) => {
     setTimeout(() => {
       app.zoomToFit({ duration: 100 });
     }, 200);
     addEventListener("resize", (event) => {
       app.zoomToFit({ duration: 100 });
     });
+    setApp(app);
   };
 
   return (
@@ -350,6 +361,7 @@ export default function Example() {
 let lastId = 0;
 
 function Main() {
+  const [proofTree, setProofTree] = useState<string | null>(null);
   useEffect(() => {
     function getTypes() {
       fetch("getTypes")
@@ -358,7 +370,12 @@ function Main() {
           const proofTree = res.data;
           const id = Number(res.id);
           if (id > lastId) {
-            console.log(id, proofTree);
+            if (proofTree.length > 0) {
+              console.log(id, proofTree[0].fromNode);
+              setProofTree(proofTree[0].fromNode);
+            } else {
+              console.log("Empty proof");
+            }
             lastId = id;
           }
         })
@@ -369,7 +386,7 @@ function Main() {
     const interval = setInterval(getTypes, 200);
     return () => clearInterval(interval);
   }, []);
-  return <Example />;
+  return <Example proofTree={proofTree} />;
 }
 
 ReactDOM.render(
