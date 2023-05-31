f41c8ced2ae4cba378e9cac4e06955a277cc4aa3
Anton Kovsharov <antonkovsharov@gmail.com>
2023-05-31 22:24:52 +0100

    Align hStack to the bottom

--- app/src/indexBrowser.tsx
+++ app/src/indexBrowser.tsx
@@ -46,17 +46,19 @@ function vStack(margin: number, ...boxes: Element[]): Element {
   };
 }
 
+// hStack aligns to the bottom
 function hStack(margin: number, ...boxes: Element[]): Element {
   if (boxes.length == 0) return { size: [0, 0], draw: () => { } };
+  const [w, h] = [
+    boxes.map((b) => b.size[0]).reduce((x, y) => x + y) + (boxes.length - 1) * margin,
+    Math.max(...boxes.map((b) => b.size[1])),
+  ];
   return {
-    size: [
-      boxes.map((b) => b.size[0]).reduce((x, y) => x + y) + (boxes.length - 1) * margin,
-      Math.max(...boxes.map((b) => b.size[1])),
-    ],
+    size: [w, h],
     draw(x, y) {
       let dx = 0;
       for (const box of boxes) {
-        box.draw(x + dx, y);
+        box.draw(x + dx, y + h - box.size[1]);
         dx += box.size[0] + margin;
       }
     },
