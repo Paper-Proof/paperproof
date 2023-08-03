3bc3088c057aab5e1cb99914dce14a3d7ce6869a
Evgenia Karunus <lakesare@gmail.com>
2023-08-03 21:13:03 +0500

    <HypNode/>, <GoalNode/> - make them readOnly

--- app/src/shapes/CustomNodeUtil.tsx
+++ app/src/shapes/CustomNodeUtil.tsx
@@ -4,6 +4,13 @@ import React from 'react';
 export default class CustomNodeUtil extends GeoShapeUtil {
   static override type = 'customNode' as const
 
+  override canResize = () => false
+  override canEdit = () => false
+  override hideSelectionBoundsFg = () => true
+  override hideSelectionBoundsBg = () => true
+  override hideRotateHandle = () => true
+  override hideResizeHandles = () => true
+
   override component(shape: TLGeoShape) {
     // Important to store it here and not later
     const superRender = super.component(shape);
@@ -13,5 +20,10 @@ export default class CustomNodeUtil extends GeoShapeUtil {
     >
       {superRender}
     </div>
+  }
+
+  override indicator(shape: TLGeoShape) {
+    // Can't return null here because of GeoShapeUtil's typings
+    return <div/>
   }
 }
