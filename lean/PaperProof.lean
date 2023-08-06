beab8025bd793073202b395f3f7acc989f96e747
Evgenia Karunus <lakesare@gmail.com>
2023-08-07 04:43:35 +0500

    parser - added thoughts about the focused state

--- lean/PaperProof.lean
+++ lean/PaperProof.lean
@@ -17,6 +17,18 @@ open Server RequestM in
 @[server_rpc_method]
 def getPpContext (params : GetPpContextParams) : RequestM (RequestTask Proof) := do
   withWaitFindSnapAtPos params.pos fun snap => do
+    -- @anton this is probably slightly cryptic, don't read
+    -- lakesare:
+    -- Just thinking about making `TacticApplication`s have `isFocused: true`.
+    -- This way hovers will be more under our control & we'll know the "focus path".
+    -- We know the focus path just via "who is a parent window of whom" either way though.
+
+    -- (partially copypasted from https://github.com/leanprover/lean4/blob/1f3ef28a1dfe903c0a62663fee4301e6da015942/src/Lean/Server/FileWorker/RequestHandling.lean#L205)
+    -- let doc ← readDoc
+    -- let text := doc.meta.text
+    -- let hoverPos := text.lspPosToUtf8Pos params.pos
+    -- let focusedGoals : List GoalsAtResult := snap.infoTree.goalsAt? text hoverPos
+
     let tree := snap.infoTree
     let th ← parse tree
     return th
