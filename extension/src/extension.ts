2b911be2248edf4c2f3c936693f2aa41952ce25a
Evgenia Karunus <lakesare@gmail.com>
2023-08-11 07:52:07 +0500

    extension - refactor 3

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -4,6 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 import { ProofState, ProofError } from "./types";
 import setupStatusBar from "./services/setupStatusBar";
 import getWebviewContent from "./services/getWebviewContent";
+import vscodeRequest from "./services/vscodeRequest";
 
 const supabaseUrl = "https://rksnswkaoajpdomeblni.supabase.co";
 const supabaseKey =
@@ -47,28 +48,6 @@ const sendTypes = async (
   if (sessionId) {
     await sendTypesToServer(sessionId, body);
   }
-};
-
-// Simple request. We don't keep session open and create a new one for each request for now.
-const vscodeRequest = async (
-  log: vscode.OutputChannel,
-  method: string,
-  client: any,
-  uri: string,
-  tdp: TextDocumentPositionParams,
-  params: any
-): Promise<any> => {
-  log.appendLine(`Making ${method} request`);
-  const connection = await client.sendRequest("$/lean/rpc/connect", { uri });
-  const response = await client.sendRequest("$/lean/rpc/call", {
-    sessionId: connection.sessionId,
-    method,
-    // These are always needed for Lean rpcs
-    // (https://github.com/leanprover/lean4/blob/367b38701fb9cca80328624a016c3b3acfd5e2cd/src/Lean/Data/Lsp/Basic.lean#L288)
-    ...tdp,
-    params,
-  });
-  return response;
 };
 
 const sendInfoAtTdp = async (
@@ -206,9 +185,7 @@ export function activate(context: vscode.ExtensionContext) {
     webviewPanel.onDidDispose(() => {
       webviewPanel = null;
     });
-    log.append(
-      "Opening webviewPanel with: " + (latestInfo as any)["statement"]
-    );
+    log.append("Opening webviewPanel with: " + (latestInfo as any)["statement"]);
     webviewPanel.webview.html = getWebviewContent(SERVER_URL, latestInfo);
   }
   context.subscriptions.push(
---
+++ extension/src/services/vscodeRequest.ts
@@ -0,0 +1,26 @@
+import * as vscode from "vscode";
+import { TextDocumentPositionParams } from "vscode-languageserver-protocol";
+
+// Simple request. We don't keep session open and create a new one for each request for now.
+const vscodeRequest = async (
+  log: vscode.OutputChannel,
+  method: string,
+  client: any,
+  uri: string,
+  tdp: TextDocumentPositionParams,
+  params: any
+): Promise<any> => {
+  log.appendLine(`Making ${method} request`);
+  const connection = await client.sendRequest("$/lean/rpc/connect", { uri });
+  const response = await client.sendRequest("$/lean/rpc/call", {
+    sessionId: connection.sessionId,
+    method,
+    // These are always needed for Lean rpcs
+    // (https://github.com/leanprover/lean4/blob/367b38701fb9cca80328624a016c3b3acfd5e2cd/src/Lean/Data/Lsp/Basic.lean#L288)
+    ...tdp,
+    params,
+  });
+  return response;
+};
+
+export default vscodeRequest;
