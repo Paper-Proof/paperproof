e89d100faeb987651e4ef39ad567ad3fded5049b
Evgenia Karunus <lakesare@gmail.com>
2023-08-15 10:43:44 +0500

    tldraw - fix zooming on initial load

--- app/src/services/updateUI.ts
+++ app/src/services/updateUI.ts
@@ -19,8 +19,12 @@ const areObjectsEqual = (a: object, b: object) => {
 };
 
 const zoomProofTree = (editor: Editor) => {
+  // This is necessary, if we don't do this zooming will work stupidly until we click on the webview tab (this makes `editor.viewportScreenBounds` correct).
+  editor.updateViewportScreenBounds();
+
   const previouslyFocusedWindow = window.zoomedWindowId && editor.getShape(window.zoomedWindowId);
   const rootWindow = editor.getShape(CreateId.window(1));
+
   const desiredWindow = previouslyFocusedWindow || rootWindow;
   if (desiredWindow) {
     zoomToWindow(editor, desiredWindow);
