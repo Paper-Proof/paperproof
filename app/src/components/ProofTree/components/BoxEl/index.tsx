93c6c76f0a144aca98c605d64e9726ba7863fa27
Evgenia Karunus <lakesare@gmail.com>
2023-12-11 06:25:42 +0400

    app - prettify goal username

--- app/src/components/ProofTree/components/BoxEl/index.tsx
+++ app/src/components/ProofTree/components/BoxEl/index.tsx
@@ -21,6 +21,13 @@ const getGoalTactic = (proofTree: ConvertedProofTree, goalNodeId: string) => {
   return goalTactic || successTactic;
 }
 
+// "a._@.Mathlib.Init.Algebra.Order._hyg.1764" => "a"
+// (https://github.com/leanprover/lean4/blob/d37bbf4292c72798afdff8bf5488df09193fde58/src/Init/Prelude.lean#L4132)
+// Note: I was doing this in the parser with `.eraseMacroScopes`, but we depend in hygienic goal usernames, might be dangerous - so I moved it here.
+const prettifyGoalUsername = (username : string) => {
+  return username.split('._@')[0];
+}
+
 const BoxEl = (props: MyProps) => {
   const childrenBoxes = props.proofTree.boxes.filter((box) => box.parentId === props.box.id);
 
@@ -61,7 +68,7 @@ const BoxEl = (props: MyProps) => {
     </div>
 
     <div className="goal-username">
-      {props.box.goalNodes[0].name}
+      {prettifyGoalUsername(props.box.goalNodes[0].name)}
     </div>
   </section>
 }
