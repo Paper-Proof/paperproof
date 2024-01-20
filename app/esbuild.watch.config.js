584fd466a7c6df660c8b49333a101559401fd1ca
Anton Kovsharov <antonkovsharov@gmail.com>
2024-01-20 15:23:23 +0000

    Remove indirection through extension for app dist

--- app/esbuild.watch.config.js
+++ app/esbuild.watch.config.js
@@ -1,18 +1,18 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
 
 let ctx = await esbuild.context({
-  entryPoints: ['src/indexBrowser.tsx'],
+  entryPoints: ["src/indexBrowser.tsx"],
   bundle: true,
-  outfile: '../extension/dist/indexBrowser.js',
+  outfile: "dist/indexBrowser.js",
   sourcemap: true,
-  platform: 'browser',
+  platform: "browser",
   loader: {
-    '.json': 'json',
-    '.css': 'css',
+    ".json": "json",
+    ".css": "css",
   },
   plugins: [],
-  logLevel: 'info',
+  logLevel: "info",
   logLimit: 0,
-})
+});
 
-await ctx.watch()
+await ctx.watch();
--- app/server.cjs
+++ app/server.cjs
@@ -1,13 +1,12 @@
 // ___Why do we need `cerver.cjs`, can't we simply server assets from within /extension?
 //    We absolutely can, and that would simplify the development setup, however turns out using `server.cjs` is the fastest way to reload development code - the fastest default way requires `Developer: Reload Window` (see https://github.com/microsoft/vscode/issues/190917 and https://code.visualstudio.com/api/get-started/your-first-extension).
-const express = require('express')
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const http = require("http");
 
 const app = express();
 http.createServer(app).listen(80, () => {
-  console.log("Listening on port 80...")
+  console.log("Listening on port 80...");
 });
 
-// Serve static files from the '/extension/dist' directory
-app.use("/", express.static(path.join(__dirname, "..", "extension", "dist")));
+app.use("/", express.static(path.join(__dirname, "dist")));
