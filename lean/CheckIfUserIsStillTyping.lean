e540b26d61b4ab9de6b86c892e11149c4029f662
Evgenia Karunus <lakesare@gmail.com>
2024-05-12 14:05:33 +0400

    lean - make paperproof work with Lean-4.8.0

--- lean/CheckIfUserIsStillTyping.lean
+++ lean/CheckIfUserIsStillTyping.lean
@@ -4,33 +4,40 @@ open Lean Server RequestM
 
 -- TODO we should move this to another file, read on this (https://leanprover.github.io/functional_programming_in_lean/hello-world/starting-a-project.html)
 def checkIfUserIsStillTyping (snap : Snapshots.Snapshot) (hoverPos : Lsp.Position) : RequestM Unit := do
-  let doc ← readDoc
-  let text := doc.meta.text
 
-  -- If we have this, then it's some syntax error:
-  --   snap.beginPos: (101, 0)
-  --   snap.endPos: (103, 0)
-  --   hoverPos: (93, 4)
-  --   => no error messages
-  let isSyntaxError := hoverPos.line < (text.utf8PosToLspPos snap.beginPos).line
+  -- old: snap.beginPos
+  if let some beginPosition := snap.stx.getPos? then
+    let doc ← readDoc
+    let text := doc.meta.text
+    let snapBegins := text.utf8PosToLspPos beginPosition
 
-  if isSyntaxError then
-    throwThe RequestError ⟨.invalidParams, "stillTyping"⟩
+    -- We won't get error messages for this,
+    -- but if we have the following arrangement, then there is some syntax error:
+    --
+    --   snapBegins: (101, 0)
+    --   hoverPos: (93, 4)
+    --
+    --    hover            snapBegins
+    -- ─────┼──────────────────┼───────
+    --      93                101
+    let isSyntaxError := hoverPos.line < snapBegins.line
 
-  let snapBegins : Lsp.Position := text.utf8PosToLspPos snap.beginPos
-  for (msg : Message) in snap.msgLog.msgs do
-    let messageHappened := text.leanPosToLspPos msg.pos
-    if messageHappened.line >= snapBegins.line then
-      -- Happens in these cases:
-      -- .data equals "unknown tactic"
-      -- .data equals "expected '(' or term"
-      -- .data equals "unknown identifier 'm'"
-      -- .data equals "tactic 'apply' failed, failed to unify..."
-      let isError := match msg.severity with
-        | MessageSeverity.error => true
-        | _ => false
-      -- Everything's fine, we'll get a nice InfoTree
-      let isSorried := (← msg.data.toString).startsWith "unsolved goals"
+    if isSyntaxError then
+      throwThe RequestError ⟨.invalidParams, "stillTyping"⟩
 
-      if isError && !isSorried then
-        throwThe RequestError ⟨.invalidParams, "stillTyping"⟩
+    for (msg : Message) in snap.msgLog.msgs do
+      let messageHappened := text.leanPosToLspPos msg.pos
+      if messageHappened.line >= snapBegins.line then
+        -- Happens in these cases:
+        -- .data equals "unknown tactic"
+        -- .data equals "expected '(' or term"
+        -- .data equals "unknown identifier 'm'"
+        -- .data equals "tactic 'apply' failed, failed to unify..."
+        let isError := match msg.severity with
+          | MessageSeverity.error => true
+          | _ => false
+        -- Everything's fine, we'll get a nice InfoTree
+        let isSorried := (← msg.data.toString).startsWith "unsolved goals"
+
+        if isError && !isSorried then
+          throwThe RequestError ⟨.invalidParams, "stillTyping"⟩
