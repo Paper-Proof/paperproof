0305975a096b4893a13028eacd228aafdb3735e3
Evgenia Karunus <lakesare@gmail.com>
2024-09-06 05:13:07 +0500

    `_examples/workshop` - decorative, fix numbering

--- _examples/workshop/3-Tutorial.lean
+++ _examples/workshop/3-Tutorial.lean
@@ -3,34 +3,34 @@ import Mathlib.Algebra.BigOperators.Fin
 import Mathlib.Tactic.GCongr
 import Paperproof
 
--- 2. What hypothesis did `simp`/`assumption`/etc. tactic use???
+-- 1. What hypothesis did `simp`/`assumption`/etc. tactic use???
 theorem simple_ex (n m : ℕ)
   (h1 : ∀ {a b : Nat}, a + b = b + a)
   (h2 : ∀ {a b : Nat}, a = b + b):
     n + m = m + n := by
   simp [h1, h2]
 
--- 4. `calc` is a transitivity thing
+-- 2. `calc` is a transitivity thing
 --    (also: again, shows what hypotheses are used)
 example {m n : ℤ} (h1 : m + 3 ≤ 2 * n - 1) (h2 : n ≤ 5) : m + 3 ≤ 9 := by
   calc
     m + 3 ≤ 2 * n - 1 := by gcongr
     _     ≤ 2 * 5 - 1 := by gcongr; norm_num
 
--- 5. Rewrites are nicely nested under each other
+-- 3. Rewrites are nicely nested under each other
 example (a b c d e f : ℕ) (h : b = e) (h₂ : e = d): (a = b) → (b = c) → (e = f) → True := by
   intros ab cd ef
   rw [h, h₂] at *
   trivial
 
--- 6. Typical mathlib proof
+-- 4. Typical mathlib proof
 --    (rw[] sequences expanded)
 theorem finFunctionFinEquiv_single11 {m n : ℕ} [NeZero m] (i : Fin n) (j : Fin m) : (finFunctionFinEquiv (Pi.single i j) : ℕ) = j * m ^ (i : ℕ) := by
   rw [finFunctionFinEquiv_apply, Fintype.sum_eq_single i, Pi.single_eq_same]
   rintro x hx
   rw [Pi.single_eq_of_ne hx, Fin.val_zero', zero_mul]
 
--- 7. Any `cases`/`induction` has a uniform interface
+-- 5. Any `cases`/`induction` has a uniform interface
 namespace OurInductives
 variable (SomeGoal: Prop)
 
@@ -55,7 +55,7 @@ theorem casesRandom (h: Random) : SomeGoal := by
   cases' h with a b c
   sorry; sorry; sorry
 
--- 8. We can see exactly what path was taken by tactic combinators
+-- 6. We can see exactly what path was taken by tactic combinators
 theorem combs (p q r : Prop) (hp : p)
   : (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) := by
   repeat (first | apply And.intro | apply Or.inl; assumption | apply Or.inr | assumption)
