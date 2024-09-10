a62b2c33b39282ff6ab680153c7bee39b41a007f
Evgenia Karunus <lakesare@gmail.com>
2024-09-10 05:27:51 +0500

    `_examples/workshop` - add `sorry`s

--- _examples/workshop/1-Demo.lean
+++ _examples/workshop/1-Demo.lean
@@ -1,15 +1,17 @@
 import Mathlib.Data.Set.Basic
 import Paperproof
 
+-- Note to self: do not explain anything, just type the tactics
 theorem commutativityOfIntersections
 (s t : Set Nat) : s ∩ t = t ∩ s := by
-  ext x
-  apply Iff.intro
+  sorry
+  -- ext x
+  -- apply Iff.intro
 
-  intro h1
-  rw [Set.mem_inter_iff, and_comm] at h1
-  exact h1
+  -- intro h1
+  -- rw [Set.mem_inter_iff, and_comm] at h1
+  -- exact h1
 
-  intro h2
-  rw [Set.mem_inter_iff, and_comm] at h2
-  exact h2
+  -- intro h2
+  -- rw [Set.mem_inter_iff, and_comm] at h2
+  -- exact h2
