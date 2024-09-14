4a140b2ca7086a5e7b96231e09bc69ac49bf0531
Evgenia Karunus <lakesare@gmail.com>
2024-09-14 06:34:09 +0400

    `_examples/workshop` - final

--- _examples/workshop/1-Demo.lean
+++ _examples/workshop/1-Demo.lean
@@ -3,15 +3,5 @@ import Paperproof
 
 -- Note to self: do not explain anything, just type the tactics
 theorem commutativityOfIntersections
-(s t : Set Nat) : s ∩ t = t ∩ s := by
+(s t : Set ℕ) : s ∩ t = t ∩ s := by
   sorry
-  -- ext x
-  -- apply Iff.intro
-
-  -- intro h1
-  -- rw [Set.mem_inter_iff, and_comm] at h1
-  -- exact h1
-
-  -- intro h2
-  -- rw [Set.mem_inter_iff, and_comm] at h2
-  -- exact h2
--- _examples/workshop/2-Tutorial.lean
+++ _examples/workshop/2-Tutorial.lean
@@ -1,33 +1,32 @@
 import Mathlib.Tactic.Cases
 import Mathlib.Tactic.Use
+import Mathlib.Data.Set.Basic
 import Paperproof
 
 -- 1. What can be do to `_ → _` goal?
 -- 2. What can we do to `_ ∧ _` hypothesis?
 theorem top : a ∧ b → a := by
   sorry
-  -- intro AB
-  -- cases' AB with A B
-  -- exact A
 
 -- 1. What can be do to `_ → _` goal?
 -- 2. What can we do to `_ ∧ _` goal?
 theorem bottom : a → b → a ∧ b := by
   sorry
-  -- intros hA hB
-  -- constructor
-  -- exact hA
-  -- exact hB
 
--- 1. What can we do to `∃ ~, ~` hypothesis?
--- 2. What can we do to `∃ ~, ~` goal?
-theorem both (p q : Nat → Prop) (h: ∃ x, p x) : ∃ x, p x ∨ q x := by
-  sorry
-  -- cases' h with xxx b
-  -- constructor -- OR
-  -- apply Exists.intro xxx
-  -- left
-  -- exact b
+-- 1. Variable scopes
+theorem commutativityOfIntersections
+(s t : Set Nat) : s ∩ t = t ∩ s := by
+  ext x
+  apply Iff.intro
+
+  intro h1
+  rw [Set.mem_inter_iff, and_comm] at h1
+  exact h1
+
+  -- intro h2
+  -- rw [Set.mem_inter_iff, and_comm] at h2
+  -- exact h2
+
 
 -- Very physical actions
 -- Very few moves to memorize
