64dcd239e936cb380357553c28b0b1ffa1020426
Evgenia Karunus <lakesare@gmail.com>
2025-01-14 08:33:32 +0500

    extension - save setting updates in "User Settings" rather than in "Workspace Settings"

--- extension/src/actions/toggleWebviewPanel/index.ts
+++ extension/src/actions/toggleWebviewPanel/index.ts
@@ -19,7 +19,10 @@ const toggleWebviewPanel = (shared: Shared) => {
       if (message.type === 'from_webview:update_settings') {
         const config = vscode.workspace.getConfiguration();
         Object.entries(message.data).forEach(([settingKey, settingValue]) => {
-          config.update(`paperproof.${settingKey}`, settingValue);
+          // ___Does it update User Settings or Workspace Settings?
+          //    User Settings.
+          //    Argument `vscode.ConfigurationTarget.Global` makes it so that we update User Settings. If we left it out, Workspace Settings would be affected.
+          config.update(`paperproof.${settingKey}`, settingValue, vscode.ConfigurationTarget.Global);
         });
       }
     });
