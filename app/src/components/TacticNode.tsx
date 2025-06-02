072ff64bdd65193385cca86f993193bdd123845c
Evgenia Karunus <lakesare@gmail.com>
2025-06-03 01:01:25 +0500

    <TacticNode/> - when we're on "...", don't highlight any other tactics

--- app/src/components/TacticNode.tsx
+++ app/src/components/TacticNode.tsx
@@ -7,14 +7,18 @@ import createArrow from "src/services/createArrow";
 import prettifyTacticText from "src/services/prettifyTacticText";
 
 const isPositionWithin = (cursor: Position, tactic: PositionStartStop): boolean => {
+  // If tactic spans many lines, it just means it's the last tactic in this proof, and Lean thinks empty lines below belong to this tactic
+  const tacticSpansManyLines = tactic.stop.line - tactic.start.line > 1;
+  const stopLine = tacticSpansManyLines ? tactic.start.line + 1 : tactic.stop.line;
+  const stopCharacter = tacticSpansManyLines ? 0 : tactic.stop.character;
   return (
     cursor.line > tactic.start.line || 
     (cursor.line === tactic.start.line && cursor.character >= tactic.start.character)
   )
   &&
   (
-    cursor.line < tactic.stop.line || 
-    (cursor.line === tactic.stop.line && cursor.character <= tactic.stop.character)
+    cursor.line < stopLine || 
+    (cursor.line === stopLine && cursor.character < stopCharacter)
   );
  };
 
@@ -25,7 +29,8 @@ interface TacticNodeProps {
   isActiveGoal?: boolean
 }
 const TacticNode = (props: TacticNodeProps) => {
-  if ((!props.tactic || props.tactic.text.includes("sorry")) && props.isActiveGoal) {
+  const isEllipsisTactic = (!props.tactic || props.tactic.text.includes("sorry")) && props.isActiveGoal;
+  if (isEllipsisTactic) {
     return <div className="active-tactic">
       {/* <img src="https://private-user-images.githubusercontent.com/7578559/264729795-58f24cf2-4336-4376-8738-6463e3802ba0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzE0NzEwMTksIm5iZiI6MTczMTQ3MDcxOSwicGF0aCI6Ii83NTc4NTU5LzI2NDcyOTc5NS01OGYyNGNmMi00MzM2LTQzNzYtODczOC02NDYzZTM4MDJiYTAucG5nP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI0MTExMyUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDExMTNUMDQwNTE5WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9ZWRkYTkxZWQ0N2QzZjhiZmI4NzFjNzZlYTc4NmQ5YTU1ODc5NzNmZTcyYmY1ZjNjODYyYjc1MDJlNzEyYjU1OSZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.hPFir-MxOhsP9OxlaQ_uOmHBTZVJozmqo7rCvGv0ZFw"/> */}
       <div className="tactic -ellipsis">...</div>
