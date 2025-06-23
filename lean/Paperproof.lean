90715834cb71c0678b9e6613dbb819caf0209970
Evgenia Karunus <lakesare@gmail.com>
2025-06-24 02:51:55 +0500

    lean - add "before_after" mode

--- lean/Paperproof.lean
+++ lean/Paperproof.lean
@@ -15,17 +15,20 @@ structure OutputParams where
 
 @[server_rpc_method]
 def getSnapshotData (params : InputParams) : RequestM (RequestTask OutputParams) := do
+  let mode := "default"
   withWaitFindSnapAtPos params.pos fun snap => do
     checkIfUserIsStillTyping snap params.pos
 
-    let parsedTree? ← BetterParser snap.infoTree
-    match parsedTree? with
-    | none => throwThe RequestError ⟨.invalidParams, "noParsedTree"⟩
-    | some parsedTree => do
+    if mode == "before_after" then
+      let hoverPos := ((← readDoc).meta.text).lspPosToUtf8Pos params.pos
+      let tacticsAtThisPosition : List Elab.GoalsAtResult := snap.infoTree.goalsAt? (← readDoc).meta.text hoverPos
+      let some tactic := tacticsAtThisPosition.head? | throwThe RequestError ⟨.invalidParams, "noGoalsAtResult"⟩
+      let some ctx := Elab.Info.updateContext? tactic.ctxInfo (Elab.Info.ofTacticInfo tactic.tacticInfo) | throwThe RequestError  ⟨.invalidParams, "couldntUpdateContext"⟩
+      let parsedTree ← parseTacticInfo ctx tactic.tacticInfo [] ∅
+      return { steps := parsedTree.steps, version := 3 }
+    else
+      let some parsedTree ← BetterParser snap.infoTree | throwThe RequestError ⟨.invalidParams, "noParsedTree"⟩
       -- This happens when we hover over something other than a theorem
       if (parsedTree.steps.length == 0) then
         throwThe RequestError ⟨.invalidParams, "zeroProofSteps"⟩
-      return {
-        steps := parsedTree.steps,
-        version := 3
-      }
+      return { steps := parsedTree.steps, version := 3 }
