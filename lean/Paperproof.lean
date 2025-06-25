7c21e25d0bd728f2e9dfe129ff017df142450abc
Evgenia Karunus <lakesare@gmail.com>
2025-06-25 08:29:11 +0500

    implicitArgs, explicitArgs, body works

--- lean/Paperproof.lean
+++ lean/Paperproof.lean
@@ -10,9 +10,18 @@ structure InputParams where
   mode: String
   deriving FromJson, ToJson
 
+structure ArgumentInfo where
+  name : String
+  type : String
+  deriving Inhabited, FromJson, ToJson
+
 structure TheoremSignature where
   name : String
   signature : String
+  instanceArgs : List ArgumentInfo := []   -- []
+  implicitArgs : List ArgumentInfo := []   -- {}
+  explicitArgs : List ArgumentInfo := []   -- ()
+  body : String := ""                      -- return type
   deriving Inhabited, FromJson, ToJson
 
 structure OutputParams where
@@ -26,17 +35,58 @@ partial def getIds : Syntax → NameSet
   | .ident _ _ nm _ => NameSet.empty.insert nm
   | _ => {}
 
+def getAllArgsWithTypes (expr : Expr) : MetaM (List ArgumentInfo × List ArgumentInfo × List ArgumentInfo × String) := do
+  Meta.forallTelescope expr fun args body => do
+    let mut lctx := LocalContext.empty
+    for arg in args do
+      let decl ← arg.fvarId!.getDecl
+      lctx := lctx.addDecl decl
+    
+    let ppCtx : PPContext := { 
+      env := ← getEnv, 
+      mctx := ← getMCtx, 
+      lctx, 
+      opts := (← getOptions).setBool `pp.fullNames true 
+    }
+    
+    let mut instanceArgs := []
+    let mut implicitArgs := []
+    let mut explicitArgs := []
+    
+    for arg in args do
+      let decl ← arg.fvarId!.getDecl
+      let typeStr ← ppExprWithInfos ppCtx decl.type
+      let argInfo := { name := decl.userName.toString, type := typeStr.fmt.pretty : ArgumentInfo }
+      
+      match decl.binderInfo with
+      | BinderInfo.instImplicit => instanceArgs := instanceArgs ++ [argInfo]
+      | BinderInfo.implicit => implicitArgs := implicitArgs ++ [argInfo]
+      | BinderInfo.strictImplicit => implicitArgs := implicitArgs ++ [argInfo]
+      | BinderInfo.default => explicitArgs := explicitArgs ++ [argInfo]
+    
+    let bodyStr ← ppExprWithInfos ppCtx body
+    return (instanceArgs, implicitArgs, explicitArgs, bodyStr.fmt.pretty)
+
 def extractTheoremSignature (ctx : ContextInfo) (goalDecl : MetavarDecl) (name : Name) : RequestM (Option TheoremSignature) := do
   try
-    let (constName, sigStr) ← ctx.runMetaM goalDecl.lctx do
+    ctx.runMetaM goalDecl.lctx do
       let resolvedName ← resolveGlobalConstNoOverloadCore name
       let constInfo ← getConstInfo resolvedName
-      let lctx := goalDecl.lctx |>.sanitizeNames.run' {options := {}}
-      let ppCtx := { (ctx.toPPContext lctx) with opts := (ctx.toPPContext lctx).opts.setBool `pp.fullNames true }
-      let typeExpr ← ppExprWithInfos ppCtx constInfo.type
+      let ppCtx := { (ctx.toPPContext (goalDecl.lctx |>.sanitizeNames.run' {options := {}})) with 
+                     opts := (ctx.toPPContext goalDecl.lctx).opts.setBool `pp.fullNames true }
+      
       let nameStr ← liftM (ppExprWithInfos ppCtx (mkConst constInfo.name))
-      return (nameStr.fmt.pretty, typeExpr.fmt.pretty)
-    return some { name := constName, signature := sigStr }
+      let typeExpr ← ppExprWithInfos ppCtx constInfo.type
+      let (instanceArgs, implicitArgs, explicitArgs, body) ← getAllArgsWithTypes constInfo.type
+      
+      return some { 
+        name := nameStr.fmt.pretty, 
+        signature := typeExpr.fmt.pretty, 
+        instanceArgs,
+        implicitArgs,
+        explicitArgs,
+        body
+      }
   catch _ => return none
 
 @[server_rpc_method]
@@ -54,8 +104,7 @@ def getSnapshotData (params : InputParams) : RequestM (RequestTask OutputParams)
       
       let parsedTree ← parseTacticInfo ctx tactic.tacticInfo [] ∅
       let theorems ← (getIds tactic.tacticInfo.stx).toList.filterMapM (extractTheoremSignature ctx goalDecl)
-      
-      return { steps := parsedTree.steps, version := 3, theorems := theorems }
+      return { steps := parsedTree.steps, version := 3, theorems }
     else
       let some parsedTree ← BetterParser snap.infoTree
         | throwThe RequestError ⟨.invalidParams, "noParsedTree"⟩
