acf5fe34725470609e2d88cd13320b1c5151bd1d
Evgenia Karunus <lakesare@gmail.com>
2025-11-11 03:44:48 +0100

    ~/_examples/ForVideos.lean - add welcome instructions for codespaces users

--- _examples/demos/ForVideos.lean
+++ _examples/demos/ForVideos.lean
@@ -1,6 +1,12 @@
 import Mathlib.Data.Set.Basic
 import Paperproof
 
+-- Welcome to paperproof!
+-- You are at the correct place.
+--
+-- 1. Wait for 3 minutes, Lean is automatically loading for you now.
+-- 2. Click on the Paperproof icon above.
+-- 3. Click on any line in this proof.
 theorem commutativityOfIntersections
 (s t : Set Nat) : s ∩ t = t ∩ s := by
   ext x
